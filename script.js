71e5ec2a6e0ef871dfb704c29fa11b69ef14629d
EpicPuppy613 <61771471+EpicPuppy613@users.noreply.github.com>
2022-05-13 09:18:38 -0700

    aaaa

--- script.js
+++ script.js
@@ -29,6 +29,12 @@ G.T.T = [];
 G.T.L = [];
 G.T.B = [];
 G.T.E = [];
+G.U = {};
+G.U.on = false;
+G.U.anim = 0;
+G.points = 250;
+G.hp = 200;
+G.maxhp = 200;
 
 class Tile {
     constructor (type, x, y, texture, direction) {
@@ -52,6 +58,22 @@ class Location {
         G.T.L.push(this);
     }
 }
+class TowerPrototype {
+    constructor (name, type, texture, turret, rotspeed, firerate, damage, range, color, offset, firewidth, fireduration) {
+        this.name = name;
+        this.type = type;
+        this.texture = texture;
+        this.turret = turret;
+        this.rotspeed = rotspeed;
+        this.firerate = firerate;
+        this.damage = damage;
+        this.range = range;
+        this.color = color;
+        this.offset = offset;
+        this.firewidth = firewidth;
+        this.fireduration = fireduration;
+    }
+}
 class Tower {
     /**
      * tower
@@ -67,7 +89,8 @@ class Tower {
      * @param {String} color 
      * @param {Number} offset 
      */
-    constructor (type, x, y, texture, turret, rotspeed, firerate, damage, range, color, offset) {
+    constructor (name, type, x, y, texture, turret, rotspeed, firerate, damage, range, color, offset, firewidth, fireduration) {
+        this.name = name;
         this.type = type;
         this.x = x;
         this.y = y;
@@ -76,24 +99,71 @@ class Tower {
         this.rotspeed = rotspeed;
         this.firerate = firerate;
         this.reload = 50/firerate;
+        this.cooldown = 0;
         this.damage = damage;
         this.level = 1;
         this.direction = 0;
         this.range = range;
         this.color = color;
         this.offset = offset;
+        this.left = false;
+        this.right = false;
+        this.width = firewidth;
+        this.duration = fireduration;
+        this.level = 1;
+        this.earned = 0;
+        this.kills = 0;
         getTile(this.x, this.y).tower = this;
         G.T.B.push(getTile(this.x, this.y).tower);
     }
     tick () {
         //find enemy
         for (const enemy of G.T.E) {
-            var difx = enemy.x - this.x * G.A.A.config.scale[0] + G.A.A.config.scale[0] / 2;
-            var dify = enemy.y - this.y * G.A.A.config.scale[1] + G.A.A.config.scale[1] / 2;
+            var difx = enemy.x * G.A.A.config.scale[0] - (this.x + 0.5) * G.A.A.config.scale[0];
+            var dify = enemy.y * G.A.A.config.scale[1] - (this.y + 0.5) * G.A.A.config.scale[1];
             if (!(difx ** 2 + dify ** 2 <= (this.range * G.A.A.config.scale[0]) ** 2)) continue;
-            var direction = Math.atan2(-dify, difx);
+            var direction = (Math.atan2(enemy.y * G.A.A.config.scale[1] - (this.y + 0.5) * G.A.A.config.scale[1], enemy.x * G.A.A.config.scale[0] - (this.x + 0.5) * G.A.A.config.scale[0]) * (180 / Math.PI)) + 90;
+            var directionDiff = direction - this.direction;
+            if (directionDiff > 180) directionDiff -= 360;
+            else if (directionDiff < -180) directionDiff += 360;
+            if (directionDiff < 0) this.left = true;
+            else this.left = false;
+            if (directionDiff > 0) this.right = true;
+            else this.right = false;
+            if (Math.abs(directionDiff) < this.rotspeed / 50) {
+                this.direction = direction;
+            } else {
+                if (this.right) this.direction += this.rotspeed / 50;
+                if (this.left) this.direction -= this.rotspeed / 50;
+                if (this.direction >= 360) {
+                    this.direction -= 360;
+                } else if (this.direction < 0) {
+                    this.direction += 360;  
+                }
+            }
+            var distance = (difx / Math.cos((direction - 90) * (Math.PI/180)));
+            var hit = [distance * Math.sin(this.direction * (Math.PI/180)), -distance * Math.cos(this.direction * (Math.PI/180))];
+            //G.D = [difx, dify, hit];
+            if (
+                hit[0] >= difx - enemy.hitbox[0]/2 && 
+                hit[0] <= difx + enemy.hitbox[0]/2 &&
+                hit[1] >= dify - enemy.hitbox[1]/2 &&
+                hit[1] <= dify + enemy.hitbox[1]/2 &&
+                this.cooldown <= 0
+            ) {
+                var prevhp = JSON.parse(JSON.stringify(enemy.hp));
+                enemy.hp -= this.damage;
+                if (enemy.hp <= 0 && prevhp > 0) {this.kills++; this.earned += enemy.reward;}
+                var offset = [this.offset * Math.sin(this.direction * (Math.PI/180)), -this.offset * Math.cos(this.direction * (Math.PI/180))];
+                G.T.P.push(new Laser(this.width, this.color, hit[0] / G.A.A.config.scale[0] + (this.x + 0.5), hit[1] / G.A.A.config.scale[1] + (this.y + 0.5),
+                    (this.x + 0.5) + offset[0] / G.A.A.config.scale[0],
+                    (this.y + 0.5) + offset[1] / G.A.A.config.scale[1],
+                    this.duration));
+                this.cooldown = this.reload;
+            }
             break;
         }
+        this.cooldown--;
     }
 }
 
@@ -114,7 +184,7 @@ class Laser {
 }
 
 class EnemyPrototype {
-    constructor (type, id, size, texture, hp, damage, speed) {
+    constructor (type, id, size, texture, hp, damage, speed, hitbox, reward) {
         this.type = type;
         this.id = id;
         this.size = size;
@@ -122,20 +192,23 @@ class EnemyPrototype {
         this.hp = hp;
         this.damage = damage;
         this.speed = speed;
+        this.hitbox = hitbox;
+        this.reward = reward;
     }
-    generate (hpmult, speedmult) {
+    generate (hpmult, speedmult, rewardmult) {
         var tile = Math.floor(Math.random() * G.A.L.S.length);
         G.T.E.push(new Enemy(
-            (G.A.L.S[tile][0] * G.A.A.config.scale[0] + this.size[0] / 2) + Math.floor(Math.random() * (G.A.A.config.scale[0] - this.size[0])),
-            (G.A.L.S[tile][1] * G.A.A.config.scale[1] + this.size[1] / 2) + Math.floor(Math.random() * (G.A.A.config.scale[1] - this.size[1])),
+            (G.A.L.S[tile][0] + this.size[0] / (2 * G.A.A.config.scale[0])) + Math.floor(Math.random() * (1 - this.size[0] / G.A.A.config.scale[0]) * G.A.A.config.scale[0])/G.A.A.config.scale[0],
+            (G.A.L.S[tile][1] + this.size[1] / (2 * G.A.A.config.scale[1])) + Math.floor(Math.random() * (1 - this.size[1] / G.A.A.config.scale[1]) * G.A.A.config.scale[1])/G.A.A.config.scale[1],
             // G.A.L.S[tile][0] * G.A.A.config.scale[0],
             // G.A.L.S[tile][1] * G.A.A.config.scale[1],
-            G.A.L.S[tile], this.type, this.size, this.texture, this.hp * hpmult, this.damage, this.speed * speedmult));
+            G.A.L.S[tile], this.type, this.size, this.texture, this.hp * hpmult, this.damage, this.speed * speedmult,
+            this.hitbox, Math.round(this.reward * rewardmult)));
     }
 }
 
 class Enemy {
-    constructor (x, y, tile, type, size, texture, hp, damage, speed) {
+    constructor (x, y, tile, type, size, texture, hp, damage, speed, hitbox, reward) {
         this.x = JSON.parse(JSON.stringify(x));
         this.y = JSON.parse(JSON.stringify(y));
         this.tile = JSON.parse(JSON.stringify(tile));
@@ -147,38 +220,45 @@ class Enemy {
         this.speed = JSON.parse(JSON.stringify(speed));
         this.direction = [0, 0];
         this.movement = 0;
+        this.hitbox = hitbox;
+        this.reward = reward;
     }
     tick () {
         if (this.movement <= 0) {
             switch (getTile(this.tile[0], this.tile[1], true).direction) {
                 case 'l':
                     this.direction = [-1, 0];
-                    this.movement = G.A.A.config.scale[0];
+                    this.movement = 1;
                     this.tile[0] -= 1;
                     break;
                 case 'r':
                     this.direction = [1, 0];
-                    this.movement = G.A.A.config.scale[0];
+                    this.movement = 1;
                     this.tile[0] += 1;
                     break;
                 case 'u':
                     this.direction = [0, -1];
-                    this.movement = G.A.A.config.scale[1];
+                    this.movement = 1;
                     this.tile[1] -= 1;
                     break;
                 case 'd':
                     this.direction = [0, 1];
-                    this.movement = G.A.A.config.scale[1];
+                    this.movement = 1;
                     this.tile[1] += 1;
                     break;
                 case 'b':
+                    G.hp -= this.damage;
                     return true;
                     break;
             }
         } else {
-            this.x += this.direction[0] * this.speed * G.A.A.config.scale[0] / 50;
-            this.y += this.direction[1] * this.speed * G.A.A.config.scale[1] / 50;
-            this.movement -= Math.abs(((this.direction[0] * this.speed * G.A.A.config.scale[0]) + (this.direction[1] * this.speed * G.A.A.config.scale[1])) / 50);
+            this.x += this.direction[0] * this.speed / 50;
+            this.y += this.direction[1] * this.speed / 50;
+            this.movement -= Math.abs(((this.direction[0] * this.speed) + (this.direction[1] * this.speed)) / 50);
+        }
+        if (this.hp <= 0) {
+            G.points += this.reward;
+            return true;
         }
     }
 }
@@ -251,6 +331,8 @@ G.canvas.addEventListener('mousemove', e => {
 });
 
 G.canvas.addEventListener('click', e => {
+    if (e.offsetY < 64) return;
+    if (e.offsetY > G.height - 192 && G.U.on) return;
     //if (G.A.loaded) G.nodes.push([Math.round(e.offsetX/G.A.A.config.scale[0])*G.A.A.config.scale[0], Math.round(e.offsetY/G.A.A.config.scale[1])*G.A.A.config.scale[1]]);
     G.A.C.X = Math.round((e.offsetX - G.O.X - G.A.A.config.scale[0]/2)/G.A.A.config.scale[0]);
     G.A.C.Y = Math.round((e.offsetY - G.O.Y - G.A.A.config.scale[1]/2)/G.A.A.config.scale[1]);
@@ -297,26 +379,35 @@ document.addEventListener('keydown', e => {
             else G.O.Y -= G.A.A.config.scale[1];
             break;
         case 'KeyB':
-            if (getTile(G.A.C.X, G.A.C.Y).type == 'platform' && getTile(G.A.C.X, G.A.C.Y).tower == null) {
-                new Tower('basic', G.A.C.X, G.A.C.Y, 'basic', 'basic-turret', 90, 1, 2, 2.5, '#007eeb');
+            if (getTile(G.A.C.X, G.A.C.Y, true).type == 'platform' && getTile(G.A.C.X, G.A.C.Y).tower == null) {
+                new Tower("Basic", 'basic', G.A.C.X, G.A.C.Y, 'basic', 'basic-turret', 90, 2, 10, 2.5, '#007eeb', 20, 2, 10);
             } 
             break;
         case 'KeyN':
-            if (getTile(G.A.C.X, G.A.C.Y).type == 'platform' && getTile(G.A.C.X, G.A.C.Y).tower == null) {
-                new Tower('sniper', G.A.C.X, G.A.C.Y, 'sniper', 'sniper-turret', 45, 0.25, 8, 4, '#00cb3e');
+            if (getTile(G.A.C.X, G.A.C.Y, true).type == 'platform' && getTile(G.A.C.X, G.A.C.Y).tower == null) {
+                new Tower("Sniper", 'sniper', G.A.C.X, G.A.C.Y, 'sniper', 'sniper-turret', 45, 0.5, 40, 4, '#00cb3e', 26, 1, 25);
             } 
             break;
         case 'KeyM':
-            if (getTile(G.A.C.X, G.A.C.Y).type == 'platform' && getTile(G.A.C.X, G.A.C.Y).tower == null) {
-                new Tower('beam', G.A.C.X, G.A.C.Y, 'beam', 'beam-turret', 1800, 10, 0.2, 1.25, '#b700dc');
+            if (getTile(G.A.C.X, G.A.C.Y, true).type == 'platform' && getTile(G.A.C.X, G.A.C.Y).tower == null) {
+                new Tower("Beam", 'beam', G.A.C.X, G.A.C.Y, 'beam', 'beam-turret', 1800, 50, 0.8, 1.25, '#b700dc', 9, 4, 2);
             } 
             break;
         case 'KeyE':
-            G.A.E.basic.generate(1, 1);
+            G.A.E.basic.generate(1, 1, 1);
             break;
+        case 'KeyX':
+        case 'Backspace':
+        case 'Delete':
+            if (getTile(G.A.C.X, G.A.C.Y, true).tower != null) {
+                if (G.T.B.indexOf(getTile(G.A.C.X, G.A.C.Y).tower) != -1) {
+                    G.T.B.splice(G.T.T.indexOf(getTile(G.A.C.X, G.A.C.Y).tower), 1);
+                    getTile(G.A.C.X, G.A.C.Y, true).tower = null;
+                }
+            }
     }
     } catch (err) {
-        G.D = err.stackz;
+        G.D = err.stack;
     } 
 });
 
@@ -376,7 +467,7 @@ function DrawM () {
     DrawTower();
     if (G.A.C.on) {
         texture = 'selected-invalid';
-        try { if (getTile(G.A.C.X, G.A.C.Y).type == 'platform') texture = 'selected' } catch {};
+        try { if (getTile(G.A.C.X, G.A.C.Y).type == 'platform') {texture = 'selected';} } catch {};
         G.PC.clearRect(0, 0, G.A.A.config.scale[0], G.A.A.config.scale[1]);
         G.PC.drawImage(
             G.A.T,
@@ -398,6 +489,7 @@ function DrawM () {
         );
         G.C.restore();
     }
+    DrawUI();
 }
 
 function DrawTrack() {
@@ -458,6 +550,14 @@ function DrawTower() {
         );
         G.C.restore();
     }
+    for (const projectile of G.T.P) {
+        G.C.strokeStyle = projectile.color;
+        G.C.lineWidth = projectile.width;
+        G.C.beginPath();
+        G.C.moveTo(projectile.x1 * G.A.A.config.scale[0] + G.O.X, projectile.y1 * G.A.A.config.scale[0] + G.O.Y);
+        G.C.lineTo(projectile.x2 * G.A.A.config.scale[1] + G.O.X, projectile.y2 * G.A.A.config.scale[1] + G.O.Y);
+        G.C.stroke();
+    }
     //RENDERING ORDER: TOWER RANGE
     for (const defense of G.T.B) {
         if (defense.x != G.A.C.X || defense.y != G.A.C.Y || !G.A.C.on) continue;
@@ -485,11 +585,45 @@ function DrawEnemy() {
             G.A.A.atlas[enemy.texture][1], 
             G.A.A.config.size[0], 
             G.A.A.config.size[1],
-            enemy.x * G.A.S - G.A.A.config.size[0] / 2 + G.O.X,
-            enemy.y * G.A.S - G.A.A.config.size[1] / 2 + G.O.Y,
+            enemy.x * G.A.A.config.scale[0] - G.A.A.config.scale[0] / 2 + G.O.X,
+            enemy.y * G.A.A.config.scale[1] - G.A.A.config.scale[1] / 2 + G.O.Y,
             G.A.A.config.scale[0],
             G.A.A.config.scale[1]);
     }
+}
+
+function DrawUI () {
+    if (getTile(G.A.C.X, G.A.C.Y, true).type == 'platform' && G.A.C.on) G.U.on = true;
+    else G.U.on = false;
+    if (G.U.on && G.U.anim < 50) G.U.anim += 10;
+    if (!G.U.on && G.U.anim > 0) G.U.anim -= 10;
+    offset = (96 * Math.sin((Math.PI / 50) * (G.U.anim - 25)) + 96)
+    G.C.fillStyle = '#444';
+    G.C.fillRect(0, 0, G.width, 48);
+    G.C.fillRect(0, G.height - offset, G.width, 192);
+    if (getTile(G.A.C.X, G.A.C.Y, true).tower == null) G.C.fillStyle = 'white';
+    else G.C.fillStyle = getTile(G.A.C.X, G.A.C.Y, true).tower.color;
+    G.C.textBaseline = 'top';
+    G.C.font = "24px 'Press Start 2P', sans-serif";
+    if (getTile(G.A.C.X, G.A.C.Y, true).tower == null && G.U.on) {
+        G.C.fillText("Platform [Lv. -]", 10, G.height - offset + 10,);
+    } else if (G.U.on) {
+        tower = getTile(G.A.C.X, G.A.C.Y, true).tower;
+        G.C.fillText(tower.name + ` [Lv. ${tower.level}]`, 10, G.height - offset + 10);
+        G.C.font = "16px 'Press Start 2P', sans-serif";
+        G.C.fillText("Kills: " + tower.kills.toLocaleString(), 10, G.height - offset + 44);
+        G.C.fillText("Earned: " + tower.earned.toLocaleString() + "p", 10, G.height - offset + 64);
+    }
+    G.C.textBaseline = 'alphabetic';
+    G.C.fillStyle = 'gold';
+    G.C.font = "24px 'Press Start 2P', sans-serif";
+    G.C.fillText(G.points.toLocaleString() + 'p', 12, 36);
+    G.C.textAlign = 'right';
+    if (G.hp / G.maxhp > 1) G.C.fillStyle = 'rgb(0, 255, ' + Math.round((G.hp / G.maxhp - 1) * 255) + ')';
+    if (G.hp / G.maxhp > 0.5 && G.hp / G.maxhp <= 1) G.C.fillStyle = 'rgb(' + Math.round(255 - (G.hp / G.maxhp - 0.5) * 2 * 255) + ', 255, 0)';
+    if (G.hp / G.maxhp <= 0.5) G.C.fillStyle = 'rgb(255, ' + Math.round((G.hp / G.maxhp) * 2 * 255) + ',0)';
+    G.C.fillText('HP: ' + Math.max(Math.round(G.hp / G.maxhp * 100), 0) + '%|' + Math.max(Math.round(G.hp), 0), G.width - 12, 36);
+    G.C.textAlign = 'left';
 }
 
 function StartLoad() {
